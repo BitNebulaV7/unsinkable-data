d7146558070fbe5eaaf700df73f905bc31408ac1
BitNebulaV7 <shivaq1994@gmail.com>
2025-11-05 12:19:59 +0330

    Fix FutureWarning: observed=False

--- src/analyse.py
+++ src/analyse.py
@@ -8,16 +8,16 @@ def survival_by_sex(df):
     """
     Returns survival rate grouped by Sex.
     """
-    return df.groupby('Sex')['Survived'].mean().round(2)
+    return df.groupby('Sex', observed=True)['Survived'].mean().round(2)
 
 def survival_by_class(df):
     """
     Returns survival rate grouped by Pclass.
     """
-    return df.groupby('Pclass')['Survived'].mean().round(2)
+    return df.groupby('Pclass', observed=True)['Survived'].mean().round(2)
 
 def survival_by_sex_class(df):
     """
     Returns survival rate grouped by Sex and Pclass.
     """
-    return df.groupby(['Sex', 'Pclass'])['Survived'].mean().round(2)
+    return df.groupby(['Sex', 'Pclass'], observed=True)['Survived'].mean().round(2)
